7f27457b09b5c79ca9c9135faed5560ffa9ebd51
MedAzizGharbi <gharbi.aziz03@gmail.com>
2024-12-08 02:24:52 +0100

    FirstCommit

---
+++ README.md
@@ -0,0 +1,60 @@
+# Making a hash table with C
+This is a small project where i am going to make a hash
+table data structure in C from scratch. The main features:
+    1)Insertion
+    2)Search
+    3)Hash Function
+    4)Delete
+    5)Rehashing
+## So what's a hash table?
+A hash table is a data structure which allows us to map
+large data to a small set of data. This is done by using
+a hash function. It is an irreversible process.
+## Structs
+So the hash table item will have two fields:
+    1) The key
+    2) The value
+The hash table itself will have three fields:
+    1)Size
+    2)Count
+    3)The items stored in the table
+## Adding an item
+To add an item we create a new item i. Then we allocate
+memory to that item and assign the key and the value.
+## Deleting an item
+We free the memory allocated previously to that item.
+## Creating a hash_table
+We create a new variable ht and we allocate memory according
+to the size of that hash_table and taking in consideration 
+the size of a single item.
+## Deleting a hash_table
+We delete all the items stored in the hash table so we can 
+free the memory allocated to those. After that we can delete
+the whole hashtable by freeing the memory allocated to it.
+## Hashing
+Hashing is a technique that maps a large set of data to a 
+small set of data. It uses a hash function for doing this 
+mapping. It is an irreversible process and we cannot find
+the original value of the key from its hashed value because
+we are trying to map a large set of data into a small set 
+of data, which may cause collisions. It is not uncommon to
+encounter collisions when mapping a large dataset into a 
+smaller one. Suppose, We have three buckets and each bucket
+can store 1L of water in it and we have 5L of water also.
+We have to put all the water in these three buckets and this
+kind of situation is known as a collision. URL shorteners are
+an example of hashing as it maps large size URL to small size.
+[W3C](https://www.geeksforgeeks.org/implementation-of-hash-table-in-c-using-separate-chaining/)
+For the hashing i will use this algorithm : 
+DJB2 Hashing Algorithm: 
+Initial Hash Value (5381):
+This value is chosen to ensure a good distribution of hash values.
+Main Loop:
+The algorithm processes each character in the string. For each 
+character, it shifts the current hash value left by 5 bits 
+(hash << 5), multiplies the result by 33 (hash * 33), and then
+
+Maybe in the future i will create my own hashing algorithm, we
+should take in consideration that a stronger hash algorithm the
+more we avoid Collisions.
+
---
+++ main.c
@@ -0,0 +1,137 @@
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+
+// STRUCTS AND STUFF
+typedef struct{
+	char* key;
+	char* value;
+}ht_item;
+typedef struct{
+	int size;
+	int count;// Number of elements in the hash table 
+	ht_item** items;
+}ht_hash_table;
+
+
+static ht_item HT_DELETED_ITEM = {NULL, NULL};
+static int HT_PRIME_1 = 7;
+static int HT_PRIME_2 = 13;
+
+
+//MANAGING THE HASHTABLE AND THE ITEMS !!!INSIDE THE HASHTABLE STRUCT NO ht_hash
+ht_item* ht_new_item(const char* k, const char* v){
+	ht_item* i = malloc(sizeof(ht_item));
+	i->key = strdup(k);
+	i->value= strdup(v);
+	return i;
+}
+ht_hash_table* ht_new(){
+	ht_hash_table* ht = malloc(sizeof(ht_hash_table));
+	ht->size = 53;
+	ht->count = 0;
+	ht->items = calloc((size_t)ht->size,sizeof(ht_item));
+	return ht;
+}
+void ht_del_item(ht_item* i){
+	free(i->key);
+	free(i->value);
+	free(i);
+}
+
+void ht_del_hash_table(ht_hash_table* ht) {
+    for (int i = 0; i < ht->size; i++) {
+        ht_item* item = ht->items[i];
+        if (item != NULL) {
+            ht_del_item(item);
+        }
+    }
+    free(ht->items);
+    free(ht);
+}
+//ht_hash
+static int ht_hash(const char* s, const int a, const int m) {
+    long hash = 0;
+    const int len_s = strlen(s);
+    for (int i = 0; i < len_s; i++) {
+        hash += (long)pow(a, len_s - (i+1)) * s[i];
+        hash = hash % m;
+    }
+    return (int)hash;
+}
+//Managing Collisions by adding a hash fouk el hash
+static int ht_get_hash(const char* s, const int num_buckets, const int attempt) {
+    const int hash_a = ht_hash(s, HT_PRIME_1, num_buckets);
+    const int hash_b = ht_hash(s, HT_PRIME_2, num_buckets);
+    return (hash_a + (attempt * (hash_b + 1))) % num_buckets;
+}
+// METHODS USED BY THE HASH TABLE
+void ht_insert(ht_hash_table* ht, const char* key , const char* value){
+	ht_item* item = ht_new_item(key, value);
+	int index = ht_get_hash(item->key, ht->size, 0);
+	ht_item* currentItem = ht->items[index];
+	int i = 1;
+	while(currentItem != NULL && currentItem != &HT_DELETED_ITEM){
+		if(strcmp(currentItem->key, item->key) == 0){
+			ht_del_item(currentItem);
+			ht->items[index] = item;
+			return;
+		}
+		index = ht_get_hash(item->key , ht->size, i);
+		currentItem = ht->items[index];
+		i++;
+	}
+	ht->items[index] = item;
+	ht->count++;
+}
+
+char* ht_search(ht_hash_table* ht, const char* key){
+	int index = ht_get_hash(key , ht->size, 0);// NLAWEJ AALA LBUCKET
+	ht_item* it = ht->items[index];// LKIT EL BUCKET
+	int i = 1;
+	while(it!=NULL){ //Bch nlawjou fwest el bucket
+		if(it!=&HT_DELETED_ITEM){
+		if(strcmp(it->key,key) == 0 ){
+			return it->value;//Lkina el value mtaa lkey
+		}
+		}
+		index = ht_get_hash(key, ht->size,i);//thashi aal collision
+		it = ht->items[index];
+		i++;
+	}
+	return NULL;
+}
+void ht_delete(ht_hash_table* ht, const char*key){
+	int index = ht_get_hash(key, ht->size, 0);
+	ht_item* it = ht->items[index];
+	int i = 1;
+	while(it!=NULL){
+		if(it != &HT_DELETED_ITEM){
+			if(strcmp(it->key, key) == 0){
+				ht_del_item(it);
+				ht->items[index] = &HT_DELETED_ITEM;
+			}
+		}
+		index = ht_get_hash(key, ht->size,i);//thashi aal collision
+		it = ht->items[index];
+		i++;
+	}
+	ht->count--;
+}
+
+int main(){
+	ht_hash_table* ht = ht_new();
+	ht_insert(ht, "Wassim" , "Fathallah");
+	ht_insert(ht, "Aziz" , "Gharbi");
+	ht_insert(ht, "Amen" , "Slema");
+	char* st;
+	printf("Aala chkoun theb tlawej? : \n");
+	scanf("%s" , st);
+		if(strcmp(st , "quit")) return 0;
+	printf("El lakab: %s \n" ,ht_search(ht, st));
+	ht_del_hash_table(ht);
+}
+
